6f16fd1147a2c6a94eb2364021716637e62fb143
Vijay0806 <vijaym08062001@gmail.com>
2023-05-21 13:23:34 +0530

    first commit

---
+++ src/Data/data.js
@@ -0,0 +1,44 @@
+export  const data = [
+    {
+        id : "100",
+        name : "VIJAY",
+        batch : "B46WD",
+        email: "vijay98@gmail.com",
+        experience : 2,
+    },
+    {
+        id : "101",
+        name : "VIJI",
+        batch : "B46WD",
+        email: "vijim@gmail.com",
+        experience : 1,
+    },
+    {
+        id : "102",
+        name : "VIGNESH",
+        batch : "B46WD",
+        email: "vikki@gmail.com",
+        experience : 1,
+    },
+    {
+        id : "103",
+        name : "MANI",
+        batch : "B46WD",
+        email: "mani@gmail.com",
+        experience : 3,
+    },
+    {
+        id : "104",
+        name : "ARUN",
+        batch : "B43WD",
+        email: "mani@gmail.com",
+        experience : 2,
+    },
+    {
+        id : "105",
+        name : "VIJAYKUMAR",
+        batch : "B46WD",
+        email: "vijaykumar@gmail.com",
+        experience : 1,
+    }
+]
